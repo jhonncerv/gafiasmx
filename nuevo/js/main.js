98753ad46d97ea7056144895c2af6284e71ea8fc
Jonathan Cervantes <jonathan@thothnet.com>
2016-02-13 19:41:16 -0600

    espacio arriva

--- nuevo/js/main.js
+++ nuevo/js/main.js
@@ -25,6 +25,29 @@ $(document).ready(function() {
 			if (nextIndex == 1) {
 				$('#menu').addClass('active');
 			}
+			if(index == 1){
+				$('#section1 .iIzq').addClass('active');
+				$('#section1 .iDer').addClass('active');
+			} else {
+				$('#section1 .iIzq').removeClass('active');
+				$('#section1 .iDer').removeClass('active');
+			}
+			if(index == 2){
+				$('#section2 .iIzq').addClass('active');
+				$('#section2 .iDer').addClass('active');
+			} else {
+				$('#section2 .iIzq').removeClass('active');
+				$('#section2 .iDer').removeClass('active');
+			}
+			if(index == 3){
+				$('#section3 .iIzq').addClass('active');
+				$('#section3 .iCen').addClass('active');
+				$('#section3 .iDer').addClass('active');
+			} else {
+				$('#section3 .iIzq').removeClass('active');
+				$('#section3 .iCen').removeClass('active');
+				$('#section3 .iDer').removeClass('active');
+			}
 			if (index == 3 && direction == 'down'){
 				$('.section').eq(index -1).removeClass('moveDown').addClass('moveUp');
 			}
